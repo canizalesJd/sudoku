23f52fc26fb38a45649d1d8908c16861d1a04d27
canizalesJd <canizalesjd@gmail.com>
2026-01-05 22:36:54 -0600

    Fixing Cors issue

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import copy
 import time
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 SIZE = 9
 CELLS_AMOUNT = SIZE * SIZE
